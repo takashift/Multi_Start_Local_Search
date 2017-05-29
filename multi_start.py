356edcd757403d5cbf501cc224613d8331a7b464
takashift <e145771@ie.u-ryukyu.ac.jp>
2017-05-30 03:52:35 +0900

    最適解を選択する部分の実装

--- multi_start.py
+++ multi_start.py
@@ -7,13 +7,22 @@ P = int(sys.argv[1])
 # 順列の数
 N = 24
 
+# 割当て問題(各A,B,C,Dそれぞれの能力)
+A = [6, 1, 9, 3]
+B = [2, 5, 7, 8]
+C = [6, 3, 5, 4]
+D = [3, 5, 2, 1]
+
+# 組み込み関数で重複無しの乱数値を得るには事前に数列のリストを作成する必要がある
 list = range(N)
 
 num = np.arange(4)
 index = np.zeros(4, dtype = np.int_)
-ans = np.zeros((10, 4, 4), dtype = np.int_)
+ans = np.zeros((P, 4, 4), dtype = np.int_)
+sum = np.zeros(P*4, dtype = np.int_)
+l = 0
 
-# 重複無しでリストから値を取得
+# 重複無しでリストからランダムに値を取得
 n = np.random.choice(list, P, replace = False)
 
 # print(i)
@@ -38,16 +47,35 @@ for i in range(P):
             num_tmp = np.delete(num_tmp, index[j])
         # print(j, num_tmp)
 
-    print("P", i,  "=", '(', ans[i,0,0],ans[i,0,1],ans[i,0,2],ans[i,0,3], ')')
+    # 目的関数の計算
+    sum[l] = A[ans[i,0,0]] + B[ans[i,0,1]] + C[ans[i,0,2]] + D[ans[i,0,3]]
+    print("P", i, "=", '(', ans[i,0,0],ans[i,0,1],ans[i,0,2],ans[i,0,3], ') Ans =', sum[l])
+    l += 1
     # if ans[0] == 1:
 
+    # 隣同士を先頭から順番に入れ替える摂動
     for k in range(3):
         ans[i,k+1] = ans[i,0]
         tmp = ans[i,k+1,0+k]
         ans[i,k+1,0+k] = ans[i,k+1,1+k]
         ans[i,k+1,1+k] = tmp
-        print("P", i, k+1, "=", '(', ans[i,k+1,0],ans[i,k+1,1],ans[i,k+1,2],ans[i,k+1,3], ')')
+        # 目的関数の計算
+        sum[l] = A[ans[i,k+1,0]] + B[ans[i,k+1,1]] + C[ans[i,k+1,2]] + D[ans[i,k+1,3]]
+        print("P", i, k+1, "=", '(', ans[i,k+1,0],ans[i,k+1,1],ans[i,k+1,2],ans[i,k+1,3], ') Ans =', sum[l])
+        l += 1
 
     print()
 
-    
+# 最小の値の取得
+min = np.min(sum)
+print("min =", min)
+
+# 最小の値のインデックスを取得
+# ！np.where の出力は2次元配列なので注意！ min[0]を使う！
+min = np.where(sum == min)
+
+for m in min[0]:
+    trns = int(m/4)
+    # 割り算の結果が int じゃないので変換する
+    print("(", ans[trns, m%4, 0], ans[trns, m%4, 1], ans[trns, m%4, 2], ans[trns, m%4, 3], ")")
+
